a8ee19c469a7d65c43706a2c9d4bca2f84df132f
Tanvin-Ahmed <tanvinahmedtouhid91@gmail.com>
2023-09-30 23:26:48 +0600

    update server url

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getDataFromLS } from "../utils/localStorage";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://technote-blog-server.vercel.app/api/",
   withCredentials: true,
 });
 
